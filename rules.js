73e129e0deac64f2a117c4166d136b46a4ecc99c
Zaxskyy <121224590+Zaxskyy@users.noreply.github.com>
2022-12-23 16:41:44 +0700

    Create rules.js

---
+++ rules.js
@@ -0,0 +1,15 @@
+let handler =  m => m.reply(`
+-Dilarang Spam Bot
+
+-Dilarang Kirim Virtex Ke Bot
+
+-Dilarang Spam Owner
+
+Melanggar Rules Saya Akan Ban Selamanya
+
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['rules']
+handler.tags = ['info']
+handler.command = /^(rules)$/i
+
+export default handler
